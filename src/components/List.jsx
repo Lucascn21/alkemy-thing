e2e9c4b93657245b88c39ed6014985cbec2cbb25
lucascn21 <lucascn21@gmail.com>
2023-04-04 12:33:43 -0300

    Redirects if no token on localstorage

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -1,8 +1,24 @@
-import React from "react";
+import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import { toast } from "react-toastify";
 export const List = () => {
   const navigate = useNavigate();
+  const token = localStorage.getItem("token");
+  useEffect(() => {
+    if (!token) {
+      toast.warning("Redirected to login, no token", {
+        position: "top-center",
+        autoClose: 5000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: "dark",
+      });
+      navigate("/");
+    }
+  });
   return (
     <>
       <h1>List</h1>
