e0d42d7b7164c8e744c9c51b5327f3abd34a64a9
lucascn21 <lucascn21@gmail.com>
2023-04-07 13:03:32 -0300

    styling with tailwindcss

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,20 +1,19 @@
 export const Footer = () => {
   return (
-    <footer>
-      <nav>
-        <ul>
-          <li>
-            <a href="https://instagram.com" rel="noopener noreferrer">
-              IG
-            </a>
-          </li>
-          <li>
-            <a href="https://facebook.com" rel="noopener noreferrer">
-              FB
-            </a>
-          </li>
-        </ul>
-      </nav>
+    <footer className="text-center bg-slate-700">
+      <ul className="flex space-x-4 bg-slate-700 justify-center">
+        <li>
+          <a href="https://instagram.com" rel="noopener noreferrer">
+            IG
+          </a>
+        </li>
+        <li>
+          <a href="https://facebook.com" rel="noopener noreferrer">
+            FB
+          </a>
+        </li>
+      </ul>
+
       <p>Copyright Alchemy Challenge</p>
     </footer>
   );
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -4,8 +4,8 @@ import { Link } from "react-router-dom";
 export const NavBar = () => {
   return (
     <header>
-      <nav>
-        <ul>
+      <nav className=" h-full bg-slate-700">
+        <ul className="flex space-x-4 justify-evenly content-center h-full place-items-center">
           <li>
             <Link to="/">Home</Link>
           </li>
