cb96d2382898266b612183e78f4ecf4ba5000bad
lucascn21 <lucascn21@gmail.com>
2023-04-07 13:02:57 -0300

    Login is now a page

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
-import { Login } from "./components/Login";
 import { Footer } from "./components/Footer";
 import { NavBar } from "./components/NavBar";
 
+import { Login } from "./pages/Login";
 import { List } from "./pages/List";
 import { Contact } from "./pages/Contact";
 
@@ -9,7 +9,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
 
 import "react-toastify/dist/ReactToastify.css";
-import "./App.css"
+import "./App.css";
 
 export default function App() {
   return (
