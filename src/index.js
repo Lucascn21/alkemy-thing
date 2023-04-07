9530487a0588cfd97db79420a53daab057648395
lucascn21 <lucascn21@gmail.com>
2023-04-07 13:03:05 -0300

    linting

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
-import ReactDOM from 'react-dom/client';
-import App from './App';
+import ReactDOM from "react-dom/client";
+import App from "./App";
 
-const container = document.getElementById('root');
+const container = document.getElementById("root");
 const root = ReactDOM.createRoot(container);
 root.render(<App />);
