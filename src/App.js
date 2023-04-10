9a1acc62793779429373908dda41f537e2f9a7cf
lucascn21 <lucascn21@gmail.com>
2023-04-10 20:05:41 -0300

    added movie page

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { NavBar } from "./components/NavBar";
 import { Login } from "./pages/Login";
 import { List } from "./pages/List";
 import { Contact } from "./pages/Contact";
+import { Movie } from "./pages/Movie";
 
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
@@ -20,6 +21,7 @@ export default function App() {
           <Route path="/" element={<Login />} />
           <Route path="/list" element={<List />} />
           <Route path="/contact" element={<Contact />} />
+          <Route path="/movie" element={<Movie />} />
         </Routes>
         <Footer />
       </Router>
