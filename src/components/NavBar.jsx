8b196e3f0f44538dc425c3257e6793abbe17059d
lucascn21 <lucascn21@gmail.com>
2023-04-10 20:06:14 -0300

    Moved logout button to navbar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,13 +1,15 @@
-import * as React from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import { toast } from "react-toastify";
 
 export const NavBar = () => {
+  const navigate = useNavigate();
+  const token = localStorage.getItem("token");
   return (
     <header>
       <nav className=" h-full bg-slate-500">
         <ul className="flex space-x-4 justify-evenly content-center h-full place-items-center">
           <li>
-            <Link to="/">Home</Link>
+            <Link to="/">Login</Link>
           </li>
           <li>
             <Link to="/list">List</Link>
@@ -15,6 +17,30 @@ export const NavBar = () => {
           <li>
             <Link to="/contact">Contact</Link>
           </li>
+
+          {token != null && (
+            <li>
+              <button
+                className="w-full bg-stone-400 px-2.5 py-1.5 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50"
+                onClick={() => {
+                  localStorage.removeItem("token");
+                  toast.success("Logged out successfully", {
+                    position: "top-center",
+                    autoClose: 5000,
+                    hideProgressBar: false,
+                    closeOnClick: true,
+                    pauseOnHover: true,
+                    draggable: true,
+                    progress: undefined,
+                    theme: "dark",
+                  });
+                  navigate("/");
+                }}
+              >
+                logout
+              </button>
+            </li>
+          )}
         </ul>
       </nav>
     </header>
--- src/pages/List.jsx
+++ src/pages/List.jsx
@@ -7,7 +7,6 @@ export const List = () => {
   const token = localStorage.getItem("token");
   useEffect(() => {
     if (!token) {
-      
       toast.warning("Log in, please", {
         position: "top-center",
         autoClose: 5000,
@@ -24,25 +23,6 @@ export const List = () => {
   });
   return (
     <main>
-      <button
-        className="w-full  bg-stone-400 px-2.5 py-1.5 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50"
-        onClick={() => {
-          localStorage.removeItem("token");
-          toast.success("Logged out successfully", {
-            position: "top-center",
-            autoClose: 5000,
-            hideProgressBar: false,
-            closeOnClick: true,
-            pauseOnHover: true,
-            draggable: true,
-            progress: undefined,
-            theme: "dark",
-          });
-          navigate("/");
-        }}
-      >
-        logout
-      </button>
       <MovieList />
     </main>
   );
