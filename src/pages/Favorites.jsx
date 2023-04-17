ccf93a6f8a42298a699ab928203591a26f609217
lucascn21 <lucascn21@gmail.com>
2023-04-17 14:05:07 -0300

    some page auth

--- src/pages/Favorites.jsx
+++ src/pages/Favorites.jsx
@@ -1,12 +1,44 @@
-import React from "react";
-
+import React, { useEffect } from "react";
 import { MovieList } from "../components/MovieList";
 
+import { useNavigate } from "react-router-dom";
+import { toast } from "react-toastify";
+
 export const Favorites = ({ addOrRemoveFav }) => {
+  const navigate = useNavigate();
+  const token = sessionStorage.getItem("token");
   const localFavsString = localStorage.getItem("favMovies");
   const localFavsArray = JSON.parse(localFavsString);
   const localFavsObj = localFavsArray?.map((fav) => {
     return JSON.parse(fav);
+  });
+  useEffect(() => {
+    if (!token) {
+      toast.warning("Log in, please", {
+        position: "top-center",
+        autoClose: 5000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: "dark",
+      });
+      navigate("/");
+    }
+    if (!localFavsArray) {
+      toast.warning("No favorites, go ❤️ some movies", {
+        position: "top-center",
+        autoClose: 5000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: "dark",
+      });
+      navigate("/popular");
+    }
   });
 
   return (
--- src/pages/Popular.jsx
+++ src/pages/Popular.jsx
@@ -1,12 +1,26 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
 import { toast } from "react-toastify";
-
+import { useNavigate } from "react-router-dom";
 import { MovieList } from "../components/MovieList";
 export const Popular = ({ addOrRemoveFav }) => {
   const [movieArray, setMovieArray] = useState([]);
-
+  const navigate = useNavigate();
+  const token = sessionStorage.getItem("token");
   useEffect(() => {
+    if (!token) {
+      toast.warning("Log in, please", {
+        position: "top-center",
+        autoClose: 5000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: "dark",
+      });
+      navigate("/");
+    }
     const endPoint = `https://api.themoviedb.org/3/movie/popular?api_key=${process.env.REACT_APP_MOVIE_API_KEY}`;
     axios
       .get(endPoint)
@@ -30,6 +44,10 @@ export const Popular = ({ addOrRemoveFav }) => {
   return movieArray.length === 0 ? (
     <h2>No results.</h2>
   ) : (
-    <MovieList listSection="Popular" addOrRemoveFav={addOrRemoveFav} movieArray={movieArray} />
+    <MovieList
+      listSection="Popular"
+      addOrRemoveFav={addOrRemoveFav}
+      movieArray={movieArray}
+    />
   );
 };
